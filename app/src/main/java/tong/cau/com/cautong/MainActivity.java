e9ae7a8fd84372621f191fc307d348f6703033a6
DESKTOP-AG5S6BO\hyob <gyqls0309j@naver.com>
2017-10-25 18:15:43 +0900

    맨처음

---
+++ app/src/main/java/tong/cau/com/cautong/MainActivity.java
@@ -0,0 +1,13 @@
+package tong.cau.com.cautong;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+	}
+}
